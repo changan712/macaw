4be50bfc2c8dcb7dc30cae77fe6c8858f4514eee
tinshee <tinshee@sohu.com>
2017-05-15 19:11:28 +0800

    init 2

--- src/app/button/button.module.ts
+++ src/app/button/button.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ButtonRoutingModule } from './button-routing.module';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [
+        CommonModule,
+        ButtonRoutingModule
+    ],
+    declarations: []
 })
 export class ButtonModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import {NgModule, SkipSelf, Optional} from '@angular/core';
+
+
+@NgModule({
+    imports: [],
+    declarations: []
+})
+export class CoreModule {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error(
+                'CoreModule is already loaded. Import it in the AppModule only');
+        }
+    }
+}
